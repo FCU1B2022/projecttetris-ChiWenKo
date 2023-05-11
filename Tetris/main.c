4d19cf3f11e17d2fa8da65735f3a4c9709248bdf
ChiWenKo <ke0905162361@gmail.com>
2023-05-11 10:56:01 +0800

    week2

--- Tetris/main.c
+++ Tetris/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define CANVAS_WIDTH 10
+#define CANVAS_HEIGHT 20
 
 typedef enum {
 	RED=41,
@@ -233,8 +239,54 @@ Shape shapes[7] = {
 	},
 };
 
+typedef struct {
+	int x;
+	int y;
+	int score;
+	int rotate;
+	int fallTime;
+	ShapeId queue[4];
+}State;
+
+typedef struct {
+	Color color;
+	ShapeId shape;
+	bool current;
+}Block;
+
+void resetBlock(Block* block) {
+	block->color = BLACK;
+	block->shape = EMPTY;
+	block->current = false;
+}
+
 int main() {
-	Color cur;
+	srand(time(NULL));
+	State state = {
+		.x = CANVAS_WIDTH / 2,
+		.y = 0,
+		.score = 0,
+		.rotate = 0,
+		.fallTime = 0
+	};
+
+	for (int i = 0; i < 4; i++)
+	{
+		state.queue[i] = rand() % 7;
+	}
+
+	Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+	for (int i = 0; i < CANVAS_HEIGHT; i++)
+	{
+		for (int j = 0; j < CANVAS_WIDTH; j++)
+		{
+			resetBlock(&canvas[i][j]);
+		}
+	}
+
+	system("cls");
+	printf("\e[?25l"); //hide cursor
+	/*Color cur;
 	for (int i = 0; i < 7; i++)
 	{
 		for (int r = 0; r < 4; r++)
@@ -259,5 +311,5 @@ int main() {
 		}
 		printf("\n");
 	}
-	return 0;
+	return 0;*/
 }
